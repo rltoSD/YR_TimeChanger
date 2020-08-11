ab6c4425e3ff25831bb88ec539034a3a6d1000b4
young.li <young.li@servicenow.com>
2020-08-11 16:28:45 -0700

    swaps time

--- background.js
+++ background.js
@@ -3,9 +3,24 @@ console.log("background running");
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab){
-    console.log("button press success");
     let msg = {
         txt: "hello"
     }
+    console.log(tab.id)
     chrome.tabs.sendMessage(tab.id, msg);
+
 }
+
+/*
+chrome.browserAction.onClicked.addListener(function() {
+    console.log("pressed");
+    
+    var s = document.createElement('script');
+    // TODO: add "script.js" to web_accessible_resources in manifest.json
+    s.src = chrome.extension.getURL('script.js');
+    s.onload = function () {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
+
+});*/
--- contentscript.js
+++ contentscript.js
@@ -1,17 +1,36 @@
 chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message, sender, sendResponse) {
-    if(message.txt === "hello"){
-        window.addEventListener("load", function() {
-            
-            var s = document.createElement('script');
-            // TODO: add "script.js" to web_accessible_resources in manifest.json
-            s.src = chrome.extension.getURL('script.js');
-            s.onload = function () {
-                this.remove();
-            };
-            (document.head || document.documentElement).appendChild(s);
+window.addEventListener("load", function() {
+    console.log("pressed");
+    
+    var s = document.createElement('script');
+    // TODO: add "script.js" to web_accessible_resources in manifest.json
+    s.src = chrome.extension.getURL('script.js');
+    s.onload = function () {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s);
 
-        });
-    }
+});
+var msg = "";
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt);
+    msg = message.txt;
 }
 
+console.log(msg);
+
+if(msg === "hello"){
+    window.addEventListener(gotMessage, function() {
+        console.log("pressed");
+        
+        var s = document.createElement('script');
+        // TODO: add "script.js" to web_accessible_resources in manifest.json
+        s.src = chrome.extension.getURL('script.js');
+        s.onload = function () {
+            this.remove();
+        };
+        (document.head || document.documentElement).appendChild(s);
+    
+    });
+}

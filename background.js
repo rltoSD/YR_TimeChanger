aa6b8bf1b122ecc902b78aa025d00670d638fd60
young.li <young.li@servicenow.com>
2020-08-11 13:39:20 -0700

    trying to add button (script execution on press)

---
+++ background.js
@@ -0,0 +1,11 @@
+console.log("background running");
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+    console.log("button press success");
+    let msg = {
+        txt: "hello"
+    }
+    chrome.tabs.sendMessage(tab.id, msg);
+}
--- contentscript.js
+++ contentscript.js
@@ -1,12 +1,17 @@
-window.addEventListener("load", function() {
-    
-    var s = document.createElement('script');
-    // TODO: add "script.js" to web_accessible_resources in manifest.json
-    s.src = chrome.extension.getURL('script.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
+chrome.runtime.onMessage.addListener(gotMessage);
+function gotMessage(message, sender, sendResponse) {
+    if(message.txt === "hello"){
+        window.addEventListener("load", function() {
+            
+            var s = document.createElement('script');
+            // TODO: add "script.js" to web_accessible_resources in manifest.json
+            s.src = chrome.extension.getURL('script.js');
+            s.onload = function () {
+                this.remove();
+            };
+            (document.head || document.documentElement).appendChild(s);
 
-});
+        });
+    }
+}
 
